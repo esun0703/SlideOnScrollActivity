58836bd23544e1b1f7c21c318196f9900cdbb72f
Emily Sun <emilysun@Emilys-MBP.fios-router.home>
2017-11-08 11:44:34 -0500

    added event listener for scroll

--- js.js
+++ js.js
@@ -1,18 +1,30 @@
-<script>
-    function debounce(func, wait = 10, immediate = true) {
-      var timeout;
-      return function() {
-        var context = this, args = arguments;
+// Select All Images Listening For
+  const sliderImages = document.querySelectorAll('.slide-in');
+  // console.log(sliderImages);
+
+  // Function that checks the slide whenever user scrolls
+  function checkSlide(e){
+    console.log(e);
+  }
+
+
+
+function debounce(func, wait = 10, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
         var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
+            timeout = null;
+            if (!immediate) func.apply(context, args);
         };
         var callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
-      };
-    }
+    };
+}
 
 
-  </script>
+// Event Handler for Scroll
+  window.addEventListener('scroll', checkSlide);
